ed870d7958531f30b6b8f3d6ed3eb9e40a4b7f50
danielrpg <daniel.fernandezgp@gmail.com>
2018-07-22 11:14:03 -0400

    Cambios de estado

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.changeTitle = this.changeTitle.bind(this);
+    this.state = {
+      title: "Hola Mundo desde Estado"
+    };
+  }
+
   render() {
-    return <h1>Hola mundo</h1>;
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <button onClick={this.changeTitle}>Cambie el titulo</button>
+      </div>
+    );
+  }
+
+  changeTitle() {
+    this.setState({
+      title: "Nuevo titulo"
+    });
   }
 }
 
