d73631248d8c4962b08f5447878340ca3711a20f
danielrpg <daniel.fernandezgp@gmail.com>
2018-07-22 23:24:23 -0400

    Cambios react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import { Router, Route, browserHistory } from "react-router";
 import "./App.css";
+
+const Home = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
 
 class App extends Component {
   render() {
-    return <h1>Hola mundo</h1>;
+    return (
+      <Router history={browserHistory}>
+        <Route path="/" component={Home} />
+        <Route path="about" component={About} />
+      </Router>
+    );
   }
 }
 
