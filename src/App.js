2c10c79d445967f20ba78130644f52f1dd0008bb
danielrpg <daniel.fernandezgp@gmail.com>
2018-07-22 23:07:09 -0400

    Incluir booststrap al proyectp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <h1>Hola mundo</h1>;
+    return (
+      <div>
+        <h1>Hola mundo</h1>
+        <Button bsStyle="primary">Primary</Button>
+      </div>
+    );
   }
 }
 
